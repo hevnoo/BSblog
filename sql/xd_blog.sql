a75971a0ab12be12b0bc8581732eadc07905a898
hevnoo <961584775@qq.com>
2022-06-26 13:31:05 +0800

    bs

---
+++ sql/xd_blog.sql
@@ -0,0 +1,16 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : aaa
+ Source Server Type    : MySQL
+ Source Server Version : 50726
+ Source Host           : localhost:3306
+ Source Schema         : xd_blog
+
+ Target Server Type    : MySQL
+ Target Server Version : 50726
+ File Encoding         : 65001
+
+ Date: 07/04/2022 22:20:48
+*/
+
